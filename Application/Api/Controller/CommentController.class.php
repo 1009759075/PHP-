ac8465c259f6130a5eca1c6a33562da265ceb436
1009759075 <1009759075@qq.com>
2017-05-16 00:42:16 +0800

    删除basemodel 添加了page_size字段接口自定义每页个数，修改了模型方法，只返回布尔值。

--- Application/Api/Controller/CommentController.class.php
+++ Application/Api/Controller/CommentController.class.php
@@ -21,11 +21,19 @@ class CommentController extends baseController{
         $option['medium_id']=I('get.medium_id');
         $option['types']=I('get.types');
         $option['page']=I('get.page',1);
+        $option['page_size']=I('get.page_size',10);
         $res=$this->commentModel->get_level1_comment($option);
-        if($res['code']<>1){
-            error($res['code'],$res['msg']);
+        if(!$res){
+            $code=$this->commentModel->getError();
+            if($code==13){
+                $msg='medium_id或者types为必填项';
+            }
+            if($code==14){
+                $msg='查不到数据内容';
+            }
+            error($code,$msg);
         }else{
-            success($res['data'],$res['msg']);
+            success($res);
         }
 
     }
--- Application/Api/Model/CommentModel.class.php
+++ Application/Api/Model/CommentModel.class.php
@@ -7,7 +7,7 @@
  */
 namespace Api\Model;
 use Think\Model;
-class CommentModel extends BaseModel{
+class CommentModel extends Model{
 
 
     //获取一级评论
@@ -15,28 +15,28 @@ class CommentModel extends BaseModel{
         $field='b.id,b.content,b.star,b.like,bb.content upper_level_content,bb.id upper_level_id,u.name user_name,u.id user_id,uu.name upper_level_user_name,uu.id upper_level_user_id';
         //参数必须是数组
         if(!is_array($option)){
-            $res=$this->Model_error_return(1001,'',self::OPTION_ERROR);
-            return $res;
+          
+            $this->error= \Api\Controller\baseController::Invalid_Arguments;//参数错误
+            return false;    //参数错误
         }
         
-        //$option['medium_id'];媒介id 电影的id，文章的id，图书的id等等
-        if(empty($option['medium_id'])){
-            $res=$this->Model_error_return(1002,'','需要的medium_id为空');
-            return $res;
+        //$option['medium_id'];媒介id 电影的id，文章的id，图书的id等等 option['types'] //类型 1.电影 2.读书 3.电视 4.音乐 5.广播
+        if(empty($option['medium_id']) || empty($option['types'])){
+           $this->error= \Api\Controller\baseController::Invalid_Arguments;//参数错误
+            return false;    //缺少必要参数
         }
-        if(empty($option['types'])){
-            $res=$this->Model_error_return(1002,'','需要的types为空');
-            return $res;
-        }
+
 
         if(empty($option['page'])){
             $option['page']=1;
+
         }
         //条件 媒介ID
         $comment_map['a.belong_id']=$option['medium_id'];
 //    $comment_map['a.types']    类型 1.电影 2.读书 3.电视 4.音乐 5.广播
         $comment_map['a.types']=$option['types'];
-       // $comment_map['b.pid']=0;//表示这是一级评论
+
+        //计算总数量
         $count=M('link_comment a')
             ->join('db_user u ON a.user_id=u.id','left')
             ->join('db_comment b ON a.comment_id=b.id','left')
@@ -44,33 +44,40 @@ class CommentModel extends BaseModel{
             ->join('db_user uu ON bb.user_id=uu.id','left')
             ->where($comment_map)
             ->count();
-        $all_page=ceil($count/self::PAGESIZE);
-        $all_num=$count;
+        $all_page=ceil($count/$option['page_size']);    //总页数
+        $all_num=$count; //总条数
+        //查询分页列表
         $list=M('link_comment a')
-            ->join('db_user u ON a.user_id=u.id','left')
-            ->join('db_comment b ON a.comment_id=b.id','left')
-            ->join('db_comment bb ON b.pid=bb.id','left')
-            ->join('db_user uu ON bb.user_id=uu.id','left')
+            ->join('db_user u ON a.user_id=u.id','left')    //查询评论人信息
+            ->join('db_comment b ON a.comment_id=b.id','left')  //查询具体评论的内容
+            ->join('db_comment bb ON b.pid=bb.id','left')       //查询该条评论是回复哪一条评论的评论内容
+            ->join('db_user uu ON bb.user_id=uu.id','left')     //查询该条评论是回复那一条评论的评论人姓名
             ->where($comment_map)
             ->field($field)
-            ->page($option['page'],self::PAGESIZE)
+            ->page($option['page'],$option['page_size'])
             ->select();
+        //如果本条是直接对媒体的评论，那么这条将没有pid，所以一下四个参数会出现空值null，而不会出现设置的默认值空字符串，需要判断
         foreach ($list as $k=>$v){
             $list[$k]['upper_level_content']= $list[$k]['upper_level_content']? $list[$k]['upper_level_content']:'';
-            $list[$k]['upper_level_id']= $list[$k]['upper_level_id']? $list[$k]['upper_level_id']:'';
             $list[$k]['upper_level_id']= $list[$k]['upper_level_id']? $list[$k]['upper_level_id']:'';
             $list[$k]['upper_level_user_id']= $list[$k]['upper_level_id']? $list[$k]['upper_level_user_id']:'';
             $list[$k]['upper_level_user_name']= $list[$k]['upper_level_user_name']? $list[$k]['upper_level_user_name']:'';
         }
-        $arr=Array(
-            'curr_page'=>(int)$option['page'],
-            'all_page'=>$all_page,
-            'all_num'=>(int)$all_num,
-            'list'=>$list,
-        );
+        if(!empty($list)){
+            //组合好返回的数据
+            $arr=Array(
+                'curr_page'=>(int)$option['page'],//当前页码
+                'all_page'=>$all_page,            //总页数
+                'all_page_num'=>(int)$all_num,         //总条数
+                'list'=>$list,                    //分页后的列表
+            );
 
-        $res=$this->Model_success_return(1,$arr,'');
-        return $res;
+            return $arr;
+        }else{
+            $this->error=\Api\Controller\baseController::NOT_EXISTS;//参数错误
+            return false;
+        }
+
     }
 
 }
